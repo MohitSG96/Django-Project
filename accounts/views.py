cbd6281247e77e139b9790062f811ad6701d70b9
mohit goyal <mohit.g.mg1996@gmail.com>
2019-08-04 15:57:51 +0530

    Added alert in registration and login. UPCOMMING -> registration and login of normal user/non-staff

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 
 def register(request):
+    if request.method == 'POST':
+        # TODO CREATE REGISTRATION LOGIC
+        messages.error(request,'Testing error messages')
+        return redirect('register')
+    else:
+        return render(request, 'accounts/register.html')
+
     return render(request,'accounts/register.html')
     
 def login(request):
+    if request.method == 'POST':
+        # TODO CREATE LOGIN LOGIC
+        pass
+    else:
+        return render(request, 'accounts/login.html')
     return render(request,'accounts/login.html')
 
 def dashboard(request):
--- btre/settings.py
+++ btre/settings.py
@@ -134,3 +134,9 @@ STATICFILES_DIRS = [
 # Media Folder settings
 MEDIA_FOLDER = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+#Message
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
