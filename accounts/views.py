3191d6f5f2be1d574ea8258cbd273801de2f609b
mohit goyal <mohit.g.mg1996@gmail.com>
2019-08-04 16:46:24 +0530

    login  added

--- accounts/views.py
+++ accounts/views.py
@@ -43,8 +43,18 @@ def register(request):
     
 def login(request):
     if request.method == 'POST':
-        # TODO CREATE LOGIN LOGIC
-        pass
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username,password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are Loggedin')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid credentails')
+            return redirect('login')
     else:
         return render(request, 'accounts/login.html')
     return render(request,'accounts/login.html')
